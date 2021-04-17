a09fcfcce97f6cc0721e2eaea9e8a49d86471140
mohamadhajj7 <63814082+mohamadhajj7@users.noreply.github.com>
2021-04-17 16:10:42 -0600

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,69 @@
+package teamProject;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.WindowConstants;
+import javax.swing.JPanel;
+
+public class GUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Red Counter");
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 600, 350);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		
+		JButton btnNewButton = new JButton("Run");
+		btnNewButton.setFocusPainted(false);
+		btnNewButton.setBounds(386, 154, 60, 30);
+		panel.setLayout(null);
+	    panel.add(btnNewButton);
+	    panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	    frame.getContentPane().add(panel);
+	    frame.setSize(500, 300);
+	    frame.setVisible(true);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel.add(btnNewButton);
+	}
+}
